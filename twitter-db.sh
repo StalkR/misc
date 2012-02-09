7e01b825db4ed1bd7187e60059cd58c2a718db54
StalkR <github@stalkr.net>
2012-02-09 10:27:54 +0100

    twitter-db.sh: cron script example on how to build a twitter db using twitter-archiver & twitter-follow

---
+++ twitter-db.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+# Cron to update twitter-db
+#   25 3 * * * cd /home/stalkr/twitter-db; flock -nox lock bash -c './db.sh > log 2>&1'
+# Ensure you already performed OAuth authentication before:
+#   twitter-archiver -o -a; twitter-follow -o -a
+
+# Your username + directory to work in
+ME=stalkr_
+DIR=~/twitter-db
+
+error() { echo "Error: $@" >&2; exit 1; }
+cd "$DIR" || error "unable to cd to $DIR"
+S=$(date '+%s')
+
+# Save following/followers
+twitter-follow -o -g "$ME" > following.lst
+twitter-follow -o -r "$ME" > followers.lst
+
+# Archive all tweets of self + following/followers + others
+mkdir -p all || error "failed mkdir all"
+{
+  echo "$ME"
+  cat following.lst followers.lst others.lst 2>/dev/null
+} | twitter-archiver -o -s "$PWD/all" -- -
+
+# Build subsets of following/followers using symlinks
+rm -rf following followers
+mkdir following followers || error "failed mkdir following followers"
+while read N; do
+  [ -f "all/$N" ] && ln -s "../all/$N" "following/$N"
+done < following.lst
+while read N; do
+  [ -f "all/$N" ] && ln -s "../all/$N" "followers/$N"
+done < followers.lst
+
+# Rebuild timeline by sorting all following tweets
+find following -not -type d -print0 | xargs -0 cat | sort -n > timeline
+
+# Execution time
+D=$[$(date '+%s') - $S]
+[ -x "$(which duration)" ] && D=$(duration $D) || D="${D}s"
+echo "Total time: $D"
