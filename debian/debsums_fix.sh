90130440d9b4aa69021f9c4b62c8b53d99c35af4
StalkR <github@stalkr.net>
2012-04-08 16:09:13 +0200

    auto fix sums architecture bug

--- debian/debsums_fix.sh
+++ debian/debsums_fix.sh
@@ -8,31 +8,70 @@
 PKGINFO="/var/lib/dpkg/info"
 
 main() {
-  local FILE NEWSUM LIST SUMS FILENOROOT OLDSUM
   if [ $# -lt 1 ]; then
     echo "Usage: $0 <file>"
     exit 1
   fi
+  debsums_fix "$1"
+}
+
+debsums_fix() {
+  local FILE SUMSFILE MATCHES
   FILE=$1
 
+  SUMSFILE=$(grep -Hrl "^[a-f0-9]\{32\}  ${FILE:1}\$" "$PKGINFO" | sort)
+  if [ -z "$SUMSFILE" ]; then
+    error "File not found in any sums files."
+  fi
+
+  MATCHES=$(echo "$SUMSFILE" | wc -l)
+  if [ $MATCHES -eq 1 ]; then
+    update_sums "$FILE" "$SUMSFILE"
+
+  elif [ $MATCHES -eq 2 ]; then
+    fix_2sums "$SUMSFILE"
+
+  else
+    error "Multiple sums for this file," \
+          "fix manually: grep \"${FILE:1}\" \"$SUMSFILE\""
+  fi
+}
+
+update_sums() {
+  local FILE SUMS NEWSUM OLDSUM
+  FILE=$1
+  SUMS=$2
   NEWSUM=$(md5sum "$FILE" | cut -d' ' -f1)
-  LIST=$(grep -Hrl "^$FILE\$" "$PKGINFO")
-  [ -z "$LIST" ] && error "File not found in any package."
-  [ $(echo "$LIST" | wc -l) -ne 1 ] && \
-    error "Multiple matches for this file." \
-          "See: grep -Hr \"$FILE\" \"$PKGINFO\""
-  SUMS=${LIST%.*}.md5sums
-  [ ! -f "$SUMS" ] && error "No $SUMS file."
-  # files do not have the / at the beginning
-  FILENOROOT=${FILE#/*}
-  OLDSUM=$(sed -n "s#\([0-9a-f]\{32\}\)  $FILENOROOT\$#\1#p" "$SUMS")
-  [ -z "$OLDSUM" ] && error "Cannot find file in sums file $SUMS."
-  [ $(echo "$OLDSUM" | wc -l) -ne 1 ] && \
-    error "Multiple sums for this file?" \
-          "See: grep \"$FILENOROOT\" \"$SUMS\""
-  [ "$OLDSUM" == "$NEWSUM" ] && error "Sums are the same."
-  sed -i "s#$OLDSUM  \(.*\)#$NEWSUM  \1#" "$SUMS"
-  echo "Sums updated."
+  OLDSUM=$(sed -n "s#\([0-9a-f]\{32\}\)  ${FILE:1}\$#\1#p" "$SUMS")
+
+  if [ -z "$OLDSUM" ] || [ $(echo "$OLDSUM" | wc -l) -ne 1 ]; then
+    error "No sum or multiples matches, should not happen here."
+
+  elif [ "$OLDSUM" = "$NEWSUM" ]; then
+    error "Sums are the same."
+
+  else
+    sed -i "s#$OLDSUM  \(.*\)#$NEWSUM  \1#" "$SUMS"
+    echo "Updated $SUMS: $OLDSUM -> $NEWSUM."
+  fi
+}
+
+fix_2sums() {
+  local SUMSFILE FILE1 FILE2 ARCH FILE
+  SUMSFILE=$1
+  FILE1=$(basename "$(echo "$SUMSFILE" | head -1)")
+  FILE2=$(basename "$(echo "$SUMSFILE" | tail -1)")
+  ARCH=$(dpkg --print-architecture)
+  if [ "${FILE1%.*}" = "${FILE2%.*}:$ARCH" ]; then
+    echo "Two matches: $FILE1 and $FILE2, known arch bug, fixing file names."
+    find "$PKGINFO" -name "${FILE1%.*}*" -print | while read FILE; do
+      mv -v "$FILE" "${FILE%:$ARCH*}${FILE#*:$ARCH}"
+    done
+    echo "Done."
+
+  else
+    error "Two matches: $FILE1 and $FILE2, fix manually."
+  fi
 }
 
 error() {
@@ -40,7 +79,6 @@ error() {
   exit 1
 }
 
-if [ "${BASH_SOURCE[0]}" == "$0" ]; then
+if [ "${BASH_SOURCE[0]}" = "$0" ]; then
   main "$@"
 fi
-
