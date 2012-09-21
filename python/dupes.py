09454f3388df8374ed71ac5ece345dc99465a88f
StalkR <github@stalkr.net>
2012-09-21 12:16:28 +0200

    dupes: algorithm improvements

--- python/dupes.py
+++ python/dupes.py
@@ -3,57 +3,23 @@
 
 Other programs doing this:
 1) fdupes: http://premium.caribe.net/~adrian2/fdupes.html
+   under optimized, reads file twice if dup (md5sum, byte by byte)
 2) fdup: https://github.com/pozdnychev/fdup/
+   under optimized, compare files 2 by 2 (read same file multiple times)
 
-1) and 2) are in C, so quite optimized, but it also means you need to compile
-it. 1) needs to compute the md5sum, which means reading the file entirely when
-it can be avoided if the first bytes are different.
-
-This version is like 2) in that it directly compares files. It's less optimum
-because of python, but also easier to run (no compilation needed).
+The algorithm I implemented tries to be efficient in term of disk reads
+and comparisons:
+1) list files (ignore symlink)
+2) easy triage with stat operation:
+    - files with same path, same dev+inode are duplicates
+    - files with different sizes are not duplicates
+3) find duplicates by working on a list of disjoint set of files, comparing N
+   potential duplicates at the same time
 """
 
+import itertools
 import os
 import sys
-
-
-class File(object):
-  """This object represents a file and provides SameAs for comparison."""
-
-  def __init__(self, path):
-    self.path = path
-    self._stat = None
-
-  @property
-  def stat(self):
-    if not self._stat:
-      self._stat = os.stat(self.path)
-    return self._stat
-
-  @property
-  def dev_inode(self):
-    return self.stat.st_dev, self.stat.st_ino
-
-  @property
-  def size(self):
-    return self.stat.st_size
-
-  def SameAs(self, file2, block=4096):
-    if self.path == file2.path:
-      return True
-    elif self.dev_inode == file2.dev_inode:
-      return True
-    elif self.size != file2.size:
-      return False
-    else:
-      f1 = open(self.path, 'r')
-      f2 = open(file2.path, 'r')
-      offset = 0
-      while offset <= self.size:
-        if f1.read(block) != f2.read(block):
-          return False
-        offset += block
-      return True
 
 
 class ProgressClock(object):
@@ -93,63 +59,139 @@ class ProgressMessage(object):
     self.fp.write('\r' + ' '*80 + '\r')
 
 
-class DupesFinder(object):
-  """Class to find duplicate files in a path."""
+class File(object):
+  """This object represents a file and provides SameAs for comparison."""
 
-  def ListFiles(self, path):
-    self.list = []  # List of File.
-    p = ProgressClock('[*] Listing files')
-    for dirpath, _, filenames in os.walk(path):
-      p.Tick()
-      for filename in filenames:
-        path = os.path.join(dirpath, filename)
-        if os.path.isfile(path):
-          self.list.append(File(path))
-    p.End()
+  def __init__(self, path):
+    self.path = path
+    self._stat = None
 
-  def FindDups(self, min_size=4096):
-    self.dups = []  # List of set of File.
-    p = ProgressMessage('[*] Finding duplicates')
-    size = len(self.list)
-    for i in xrange(size - 1):  # Last one already compared with all others.
-      p.Tick('%3i%% [%i/%i] %i dups' % (100*i/size, i, size, len(self.dups)))
-      file1 = self.list[i]
-      if self.IsDup(file1):  # Already analyzed.
-        continue
-      for file2 in self.list:
-        if file1 == file2:
-          continue
-        if file1.SameAs(file2):
-          self.AddDup(file1, file2)
-      if self.IsDup(file1) and file1.size > min_size:
-        self.ReportDup(file1)
-    p.End()
+  @property
+  def stat(self):
+    if not self._stat:
+      self._stat = os.stat(self.path)
+    return self._stat
 
-  def IsDup(self, file1):
-    for dup in self.dups:
-      if file1 in dup:
+  @property
+  def dev_inode(self):
+    return self.stat.st_dev, self.stat.st_ino
+
+  @property
+  def size(self):
+    return self.stat.st_size
+
+  def ObviousDuplicate(self, file2):
+    return self.path == file2.path or self.dev_inode == file2.dev_inode
+
+  def PotentialDuplicate(self, file2):
+    return self.size == file2.size
+
+
+def ListFiles(path):
+  files = []  # List of File.
+  p = ProgressClock('[*] Listing files')
+  for dirpath, _, filenames in os.walk(path):
+    p.Tick()
+    for filename in filenames:
+      path = os.path.join(dirpath, filename)
+      if os.path.isfile(path):
+        files.append(File(path))
+  p.End()
+  return files
+
+
+class DupeList(object):
+  """Class representing a list of duplicates with helpers."""
+
+  def __init__(self, duplist=None):
+    # List of disjoint set of Files.
+    self.duplist = duplist if duplist else []
+
+  def __repr__(self):
+    return 'DupeList(%s)' % repr(self.duplist)
+
+  def __len__(self):
+    return len(self.duplist)
+
+  def __iter__(self):
+    return self.duplist.__iter__()
+
+  def Insert(self, a_set):
+    for dupset in self.duplist:
+      if dupset.intersection(a_set):
+        dupset.update(a_set)
+        return
+    self.duplist.append(a_set)
+
+  def Merge(self, a_duplist):
+    for a_dupset in a_duplist:
+      self.Insert(a_dupset)
+
+  def AddDup(self, a, b):
+    self.Insert(set([a, b]))
+
+  def IsDup(self, element):
+    for dupset in self.duplist:
+      if element in dupset:
         return True
 
-  def AddDup(self, file1, file2):
-    for dup in self.dups:
-      if file1 in dup or file2 in dup:
-        dup.update(set([file1, file2]))
-        return
-    self.dups.append(set([file1, file2]))
 
-  def ReportDup(self, file1):
-    sys.stderr.write('\n')
-    for dup in self.dups:
-      if file1 in dup:
-        for phile in dup:
-          print '%i: %r' % (phile.size, phile.path)
+def FindDups(files, block=4096):
+  """Find duplicate files in a list of File elements, two passes."""
+  dups = DupeList()
+  work = DupeList()
+
+  p = ProgressMessage('[*] First pass, triage obvious dups and not dups')
+  # Binomial coefficient C(len(files), 2)
+  total = len(files) * (len(files) - 1) / 2
+  i = 0
+  for file1, file2 in itertools.combinations(files, 2):
+    p.Tick('%3i%% [%i/%i]' % (100*i/total, i, total))
+    if file1.ObviousDuplicate(file2):
+      dups.AddDup(file1, file2)
+    elif file1.PotentialDuplicate(file2):
+      work.AddDup(file1, file2)
+    i += 1
+  p.End()
+
+  p = ProgressMessage('[*] Second pass, find duplicates in same-size files')
+  total = len(work)
+  i = 0
+  for fileset in work:
+    p.Tick('%3i%% [%i/%i] %i dups' % (100*i/total, i, total, len(dups)))
+    for f in fileset:
+      f.fp = open(f.path, 'rb')
+    dups.Merge(FindDupsRecurse(DupeList([fileset]), 0, block))
+    for f in fileset:
+      f.fp.close()
+  p.End()
+
+  return dups
 
 
-def FindDups(path, min_size=4096):
-  """Method to find and report duplicates in a path."""
-  df = DupesFinder()
-  df.ListFiles(path)
-  df.FindDups(min_size)
+def FindDupsRecurse(duplist, offset=0, block=4096):
+  """Takes a duplist of File elements and returns a sub duplist of these."""
+  if not len(duplist):
+    return []
+  if all([offset >= f.size for dupset in duplist for f in dupset]):
+    return duplist
+  sublist = DupeList()
+  for dupset in duplist:
+    for f in dupset:
+      f.block = f.fp.read(block) if offset < f.size else True
+  for dupset in duplist:
+    for file1, file2 in itertools.combinations(dupset, 2):
+      if file1.block == file2.block:
+        sublist.AddDup(file1, file2)
+  return FindDupsRecurse(sublist, offset+block, block)
+
+
+def ReportDups(duplist, min_size=4096):
+  for dupset in duplist:
+    if list(dupset)[0].size > min_size:
+      print 'Duplicates:'
+      for f in dupset:
+        print '  %i: %r' % (f.size, f.path)
 
 
 def main():
@@ -157,8 +199,9 @@ def main():
     print 'Usage: %s <path>' % sys.argv[0]
     raise SystemExit
 
-  FindDups(sys.argv[1])
-
+  files = ListFiles(sys.argv[1])
+  dups = FindDups(files)
+  ReportDups(dups)
 
 if '__main__' == __name__:
   main()
