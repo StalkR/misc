dde05e0d7d5efd28ad5a8b91d28b9ce115254398
StalkR <stalkr@stalkr.net>
2013-11-09 23:24:26 +0100

    remove imdb Python API and AppEngine app

--- imdb/appengine/appengine_config.py
+++
@@ -1,5 +0,0 @@
-def webapp_add_wsgi_middleware(app):
-    # https://developers.google.com/appengine/docs/python/tools/appstats
-    from google.appengine.ext.appstats import recording
-    app = recording.appstats_wsgi_middleware(app)
-    return app
