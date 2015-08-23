ba67f5e94d1b1c2cd550cf310b716c0a8101d7a0
StalkR <stalkr@stalkr.net>
2015-08-23 02:11:03 +0200

    python/snappy: add compress

--- python/snappy_pure.py
+++ python/snappy_pure.py
@@ -1,13 +1,16 @@
 """Snappy compression pure python implementation.
 
-The implementation is based on Snappy-Go (https://code.google.com/p/snappy-go).
-TODO: encode."""
+The implementation is based on https://github.com/golang/snappy
+at commit 723cc1e459b8eea2dea4583200fd60757d40097a."""
 
 class Error(Exception):
   """Base error class for snappy module."""
 
 class ErrorCorrupt(Error):
   """Corrupt input."""
+
+class ErrorTooLarge(Error):
+  """Decoded block is too large."""
 
 class ErrorUnsupported(Error):
   """Unsupported input."""
@@ -51,6 +54,9 @@ chunkTypeCompressedData   = 0x00
 chunkTypeUncompressedData = 0x01
 chunkTypePadding          = 0xfe
 chunkTypeStreamIdentifier = 0xff
+
+def uint8(n):
+  return n & ((1<<8) - 1)
 
 def uint32(n):
   return n & ((1<<32) - 1)
@@ -103,15 +109,24 @@ def uvarint(buf):
     s += 7
   return 0, 0
 
+def putuvarint(x):
+  """putuvarint encodes a uint64."""
+  buf = []
+  while x >= 0x80:
+    buf.append(uint8(x) | 0x80)
+    x >>= 7
+  buf.append(x)
+  return ''.join(chr(c) for c in buf)
+
 def decodedLen(src):
   """decodedLen returns the length of the decoded block and the number of bytes
   that the length header occupied."""
-  blockLen, headerLen = uvarint(src)
-  if headerLen == 0:
+  v, n = uvarint(src)
+  if n <= 0 or v > 0xffffffff:
     raise ErrorCorrupt
-  if uint64(blockLen) != blockLen:
-    raise Error('decoded block is too large')
-  return blockLen, headerLen
+  if v > 0x7fffffff:
+    raise ErrorTooLarge
+  return v, n
 
 def decompress(buf):
   """decompress returns the decompressed form of buf."""
@@ -182,3 +197,110 @@ def decompress(buf):
   if d != dLen:
     raise ErrorCorrupt
   return ''.join(chr(c) for c in dst[:d])
+
+maxOffset = 1 << 15
+
+def emitLiteral(lit):
+  """emitLiteral returns a literal chunk."""
+  n = len(lit) - 1
+  dst = []
+  if n < 60:
+    dst.append((uint8(n)<<2) | tagLiteral)
+  elif n < (1<<8):
+    dst.append((60<<2) | tagLiteral)
+    dst.append(uint8(n))
+  elif n < (1<<16):
+    dst.append((61<<2) | tagLiteral)
+    dst.append(uint8(n))
+    dst.append(uint8(n >> 8))
+  elif n < (1<<24):
+    dst.append((62<<2) | tagLiteral)
+    dst.append(uint8(n))
+    dst.append(uint8(n >> 8))
+    dst.append(uint8(n >> 16))
+  elif uint64(n) < (1<<32):
+    dst.append((63<<2) | tagLiteral)
+    dst.append(uint8(n))
+    dst.append(uint8(n >> 8))
+    dst.append(uint8(n >> 16))
+    dst.append(uint8(n >> 24))
+  else:
+    raise Error("Source buffer is too long")
+  dst.extend(lit)
+  return dst
+
+def emitCopy(offset, length):
+  """emitCopy writes a copy chunk and returns the number of bytes written."""
+  dst = []
+  while length > 0:
+    x = length - 4
+    if 0 <= x and x < (1<<3) and offset < (1<<11):
+      dst.append(((uint8(offset>>8)&0x07)<<5) | (uint8(x)<<2) | tagCopy1)
+      dst.append(uint8(offset))
+      break
+
+    x = length
+    if x > (1<<6):
+      x = 1 << 6
+    dst.append((uint8(x-1)<<2) | tagCopy2)
+    dst.append(uint8(offset))
+    dst.append(uint8(offset >> 8))
+    length -= x
+  return dst
+
+def compress(buf):
+  """compress returns the compressed form of buf."""
+  src = [ord(c) for c in buf]
+
+  # The block starts with the varint-encoded length of the decompressed bytes.
+  dst = [ord(c) for c in putuvarint(len(src))]
+
+  # Return early if src is short.
+  if len(src) <= 4:
+    if len(src) != 0:
+      dst.extend(emitLiteral(src))
+    return dst
+
+  # Initialize the hash table. Its size ranges from 1<<8 to 1<<14 inclusive.
+  maxTableSize = 1 << 14
+  shift, tableSize = 32-8, 1<<8
+  while tableSize < maxTableSize and tableSize < len(src):
+    shift -= 1
+    tableSize *= 2
+  table = [0]*maxTableSize
+
+  # Iterate over the source bytes.
+  s = 0    # The iterator position.
+  t = 0    # The last position with the same hash as s.
+  lit = 0  # The start position of any pending literal bytes.
+  while s+3 < len(src):
+    # Update the hash table.
+    b0, b1, b2, b3 = src[s], src[s+1], src[s+2], src[s+3]
+    h = uint32(b0) | (uint32(b1)<<8) | (uint32(b2)<<16) | (uint32(b3)<<24)
+    p = uint32(h*0x1e35a7bd)>>shift
+    # We need to to store values in [-1, inf) in table. To save
+    # some initialization time, (re)use the table's zero value
+    # and shift the values against this zero: add 1 on writes,
+    # subtract 1 on reads.
+    t, table[p] = table[p]-1, s+1
+    # If t is invalid or src[s:s+4] differs from src[t:t+4], accumulate a literal byte.
+    if t < 0 or s-t >= maxOffset or b0 != src[t] or b1 != src[t+1] or b2 != src[t+2] or b3 != src[t+3]:
+      s += 1
+      continue
+    # Otherwise, we have a match. First, emit any pending literal bytes.
+    if lit != s:
+      dst.extend(emitLiteral(src[lit:s]))
+    # Extend the match to be as long as possible.
+    s0 = s
+    s, t = s+4, t+4
+    while s < len(src) and src[s] == src[t]:
+      s += 1
+      t += 1
+    # Emit the copied bytes.
+    dst.extend(emitCopy(s-t, s-s0))
+    lit = s
+
+  # Emit any final pending literal bytes and return.
+  if lit != len(src):
+    dst.extend(emitLiteral(src[lit:]))
+  return ''.join(chr(c) for c in dst)
--- python/snappy_pure_test.py
+++ python/snappy_pure_test.py
@@ -1,34 +1,52 @@
 """Unit tests for Snappy compression pure python implementation.
 
-Compare with https://github.com/andrix/python-snappy which are bindings
-on top of the C++ implementation https://code.google.com/p/snappy/."""
+Rather than copying the tests at https://github.com/golang/snappy we just make
+sure we can compress/decompress against the reference bindings.
+
+Snappy reference implementation in C++: https://code.google.com/p/snappy/
+Python bindings: https://github.com/andrix/python-snappy
+"""
 
 import random
 import snappy
 import snappy_pure
 import unittest
 
+
 def randBytes(n):
   with open('/dev/urandom', 'r') as f:
     return f.read(n)
 
-class TestDecode(unittest.TestCase):
+
+class TestDecompress(unittest.TestCase):
 
   def testFile(self):
-    data = open('/etc/passwd', 'r').read()
-    compressed = snappy.compress(data)
-    if data != snappy.decompress(compressed):
-      raise AssertionError('C++ implementation faulty')
-    if data != snappy_pure.decompress(compressed):
-      raise AssertionError('Python implementation faulty')
+    want = open('/etc/passwd', 'r').read()
+    got = snappy_pure.decompress(snappy.compress(want))
+    if got != want:
+      raise AssertionError('got %r, want %r' % (got, want))
 
   def testRandom(self):
-    data = randBytes(random.randint(1, 1<<20))  # 1B to 1 MB
-    compressed = snappy.compress(data)
-    if data != snappy.decompress(compressed):
-      raise AssertionError('C++ implementation faulty')
-    if data != snappy_pure.decompress(compressed):
-      raise AssertionError('Python implementation faulty')
+    want = randBytes(random.randint(1, 1<<20))  # 1B to 1 MB
+    got = snappy_pure.decompress(snappy.compress(want))
+    if got != want:
+      raise AssertionError('got %r, want %r' % (got, want))
+
+
+class TestCompress(unittest.TestCase):
+
+  def testFile(self):
+    want = open('/etc/passwd', 'r').read()
+    got = snappy.decompress(snappy_pure.compress(want))
+    if got != want:
+      raise AssertionError('got %r, want %r' % (got, want))
+
+  def testRandom(self):
+    want = randBytes(random.randint(1, 1<<20))  # 1B to 1 MB
+    got = snappy.decompress(snappy_pure.compress(want))
+    if got != want:
+      raise AssertionError('got %r, want %r' % (got, want))
+
 
 if __name__ == '__main__':
   unittest.main()
